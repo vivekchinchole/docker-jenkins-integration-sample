3695df99d273d8acae59026062b8713ff38b2bbb
harsha-demo <vivekchinchole98@gmail.com>
2025-11-13 11:25:38 +0530

    updated

--- src/main/java/com/example/springboot_K8s_example/SpringbootK8sExampleApplication.java
+++ src/main/java/com/example/springboot_K8s_example/SpringbootK8sExampleApplication.java
@@ -2,10 +2,16 @@ package com.example.springboot_K8s_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootK8sExampleApplication {
-
+	@GetMapping
+	public String message(){
+		return "welcome to javatechie";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootK8sExampleApplication.class, args);
 	}
